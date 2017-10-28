27fddefeb8e52a1d9776fb565371498f37da7a9f
Logan Barnes <barneslt63@gmail.com>
2017-10-28 16:02:05 +0100

    Test OpenGLSimulation compilation with simple unit tests

---
+++ src/testing/TemplateCompilationTests.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include <sim-driver/OpenGLSimulation.hpp>
+
+namespace
+{
+struct FirstSim
+{
+};
+
+struct SecondSim
+{
+    SecondSim(int, int)
+    {}
+};
+
+struct ThirdSim
+{
+    ThirdSim(int, int, sim::SimData *)
+    {}
+};
+
+} // namespace
+
+TEST(OpenGLSimulationCompiles, compiles_with_default_constructor)
+{
+    sim::OpenGLSimulation<FirstSim> sim;
+    EXPECT_TRUE(true);
+}
+
+TEST(OpenGLSimulationCompiles, compiles_with_width_height_constructor)
+{
+    sim::OpenGLSimulation<SecondSim> sim;
+    EXPECT_TRUE(true);
+}
+
+TEST(OpenGLSimulationCompiles, compiles_with_SimData_constructor)
+{
+    sim::OpenGLSimulation<ThirdSim> sim;
+    EXPECT_TRUE(true);
+}
